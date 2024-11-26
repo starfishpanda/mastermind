de752120e28fd74ade2b5088ce6c59598038fdd7
Philip Brown <philip.murga@gmail.com>
2024-11-25 17:16:21 -0800

    test

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,27 +1,26 @@
 import React from 'react';
 import App from './App';
 import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 
-const router = createBrowserRouter([
-  {
-    path: '*',
-    element: <App />,
-  }
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: '*',
+//     element: <App />,
+//   }
+// ]);
 
 const rootDiv = document.getElementById('root') as HTMLElement;
 const root = createRoot(rootDiv);
 
 root.render(
-  // <React.StrictMode>
-  <>
-    <ToastContainer position="top-right" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
-    <RouterProvider router={router}/>
-  </>
-   
-  // </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <ToastContainer position="top-right" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
