aef625d5180944832d621f86d0e634ba30ae5c23
Philip Brown <philip.murga@gmail.com>
2025-03-25 12:13:50 -0700

    Clean backend and frontend.

--- frontend/src/components/Login/Home.tsx
+++ frontend/src/components/Login/Home.tsx
@@ -27,7 +27,6 @@ export default function Home() {
           >
             Play
           </Button>
-          
           {!isAuthenticated ? (
             <>
               <Button
--- frontend/src/components/Login/VerifyEmail.tsx
+++ frontend/src/components/Login/VerifyEmail.tsx
@@ -34,4 +34,4 @@ export default function VerifyEmail() {
       </div>
     </div>
   )
-} 
+}
